6c3a3e619cf36913e1b547bc3a18c750748e887a
Manuel Fill <ap0calypse@agitatio.org>
2011-08-31 12:36:41 +0300

    fixed the usage text

--- saiyan.c
+++ saiyan.c
@@ -26,7 +26,7 @@ int main(int argc, char *argv[]) {
         usage();
         exit(1);
     }
-    // INPUTFILE given
+    // INPUTFILE and OUTPUTFILE given
     else if (argc == 3) {
         process_file(argv[1], argv[2]);
     }
@@ -39,10 +39,9 @@ int main(int argc, char *argv[]) {
 
 void usage() {
     printf("\nUsage information:\n\n " \
-           "\tsaiyan INPUTFILE [OUTPUTFILE]\n\n" \
+           "\tsaiyan INPUTFILE OUTPUTFILE\n\n" \
             "\t\tWhere INPUTFILE is the path to a plain or ciphered text file,\n" \
-            "\t\tand OUTPUTFILE is optional. If you don't specify OUTPUTFILE, output\n" \
-            "\t\twill be sent to STDOUT.\n\n");
+            "\t\tand OUTPUTFILE is where the resulting de-/encrypted file will go.\n\n");
 }
 
 void process_file(char *infile, char *outfile) {
