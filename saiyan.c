93381e5614ef42104f1a67d7a6362f8dff564869
Manuel Fill <ap0calypse@agitatio.org>
2011-09-01 08:58:51 +0200

    removed obsolete header files
    Signed-off-by: Manuel Fill <ap0calypse@agitatio.org>

--- saiyan.c
+++ saiyan.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
-#include <ctype.h>
 #include <unistd.h>
-#include <string.h>
 #include <stdlib.h>
 
 /******************************************************************************
